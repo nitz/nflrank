6616db8ca2da5c4b582c4b27c9014071738d0b99
Chris Dailey <nitz@users.noreply.github.com>
2023-09-14 21:35:09 -0400

    Bump major.

--- main.php
+++ main.php
@@ -2,9 +2,9 @@
 declare(strict_types=1);
 
 final class ApplicationVersion {
-	public const /*int*/ MAJOR = 1;
+	public const /*int*/ MAJOR = 2;
 	public const /*int*/ MINOR = 0;
-	public const /*int*/ PATCH = 2;
+	public const /*int*/ PATCH = 0;
 	public const VERSION = "v" . self::MAJOR . "." . self::MINOR . "." . self::PATCH;
 
 	public static function get(): string {
